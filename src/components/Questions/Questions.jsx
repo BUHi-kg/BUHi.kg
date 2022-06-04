abebb1ffc7e65491929092ba6b6316cd2d4f4044
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2022-06-04 21:53:38 +0600

    add block six, questions

---
+++ src/components/Questions/Questions.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import './Questions.css';
+import { Form, Input, Button } from 'antd';
+import leftHalfCircle from '../../images/lefthalfCircle.png';
+import cubes from '../../images/Cubes.png'
+
+function Questions() {
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className='questions'>
+      
+      <div>
+        <img src={leftHalfCircle} className='bg_halfcircle'/>
+        <img src={cubes} className='bg_cubes' />
+      </div>
+
+      <div className='questions_title'>
+          <h2>Остались вопросы?</h2>
+        <div className='questions_title__subtitle'>
+          <p>Оставьте контакты - поможем решить вашу задачу и проконсультируем по спорным вопросам</p>
+        </div>
+      </div>
+
+      <div className='questions_inputform'>
+
+        <Form
+          name="basic"
+          wrapperCol={{
+            span: 16,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input 
+              className='input-name'
+              placeholder='Name'
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="tel"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your tel!',
+              },
+            ]}
+          >
+            <Input.Group>
+              <Input
+                className='input-int-code'
+                defaultValue="+(996)"
+              />
+              <Input
+                className='input-phone'
+                placeholder="(000)00-00-00"
+              />
+            </Input.Group>
+          </Form.Item>
+
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit" className='input-submit'>
+            Оставить заявку
+            </Button>
+          </Form.Item>
+        </Form>
+
+      </div>
+    </div>
+  )
+}
+
+export default Questions
