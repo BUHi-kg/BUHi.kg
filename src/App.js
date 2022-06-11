eb33f9baed4425a43e88d76160101a4ce8995a84
Islam52 <islamhihiza11@gmail.com>
2022-06-11 11:10:02 +0600

    islam

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Header from './components/BlockFirst/Header/Header';
 import Footer from './components/BlockFooter/Footer/Footer';
-import FormFSC from './components/BlockFirst/FormFSC/FormFSC'
-import React from 'react'
+import FormFSC from './components/BlockFirst/FormFSC/FormFSC';
 import 'antd/dist/antd.css'
 import Faq from './components/FAQ/Faq';
 import HowItWorks from './components/HowItWorks/HowItWorks';
